a16c4c70f39e0894c69d5cb47e8ec605c89a9507
John Mohlenkamp <john@mohlenkamp.com>
2021-08-31 17:25:51 -0500

    updating project modal and portfolio. Used wrong branch

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Project from '../Project';
 
 function Portfolio () {
+
+
 
     return(
         <section>
             <h1>Portfolio</h1>
+            <Project></Project>
         </section>
     )
 }
